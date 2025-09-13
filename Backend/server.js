78d3c5f3845388efd91d3a88958dbf9da4b4fba7
srinath2404 <srinathgaddam41@gmail.com>
2025-09-13 08:55:17 +0530

    Added CORS

--- Backend/server.js
+++ Backend/server.js
@@ -22,7 +22,7 @@ const priceChartRoutes = require("./routes/priceChartRoutes");
 const app = express();
 
 app.use(cors({
-  origin: ['https://sbpi-frontend.vercel.app', 'http://localhost:3000'],
+  origin: ['https://sbpi-htaa.vercel.app', 'http://localhost:3000'],
   credentials: true
 }));
 app.use(helmet({
